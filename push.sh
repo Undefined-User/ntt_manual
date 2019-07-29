254ce8914e541827dd8ddf92bf1ab0f77502c6b4
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-29 22:30:53 +0800

    喵

--- push.sh
+++ push.sh
@@ -1,13 +1,3 @@
-rm -rf .git
-
-git init
-
-git config --global user.name "稗田 奈间"
-git config --global user.email "HiedaNaKan@kurumi.io"
-git config credential.helper store
-
-git remote add origin https://github.com/HiedaNaKan/ntt_manual
-
 git add .
 
 if [ ! $1 ]; then
@@ -20,4 +10,4 @@ else
   
 fi
 
-git push origin master --force
+git push origin master
